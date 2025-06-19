10041c3f5839c526e04d2bf254ea26f900ddfda5
emanuelbrits <emanuelbritoss@outlook.com>
2025-06-19 13:41:18 -0300

    refactor(login): improve mobile layout and visual hierarchy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { InvestimentosComponent } from './investimentos/investimentos.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth.guard';
-import { LoginComponent } from './login.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [{ path: '', component: HomeComponent, canActivate: [AuthGuard] },
 { path: 'investimentos', component: InvestimentosComponent, canActivate: [AuthGuard] },
