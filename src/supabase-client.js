acde92162fe430917654ac4a273c4a71160a779a
emanuelbrits <emanuelbritoss@outlook.com>
2025-06-25 00:17:07 -0300

    fix

--- src/supabase-client.js
+++ src/supabase-client.js
@@ -1,7 +1,7 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://kkymclhbftvzcifshyai.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtreW1jbGhiZnR2emNpZnNoeWFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzIzODA4MzgsImV4cCI6MjA0Nzk1NjgzOH0.auWWiZlPUc-EVj4pAVg7FJXOUCAlTyFcAWcPMdzCkrs';
+const supabaseUrl = environment.SUPABASE_URL;
+const supabaseKey = environment.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 module.exports = { supabase };
